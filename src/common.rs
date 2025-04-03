cd628c5ee5510de3c52945b1071ff44af668250a
Ye-Hub2 <1292252535@qq.com>
2025-04-03 17:47:29 +0800

    Update common.rs

--- src/common.rs
+++ src/common.rs
@@ -944,7 +944,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "http://192.168.200.10".to_owned()
+    "http://192.168.200.10:21114".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
